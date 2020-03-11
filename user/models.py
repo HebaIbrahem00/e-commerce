4cf3a467c11851a6f852c93e8bf8cad650eb9169
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2020-03-11 12:40:56 +0200

    reviews updated

--- user/models.py
+++ user/models.py
@@ -27,12 +27,22 @@ class Favourite(models.Model):
 
 
 class Reviews(models.Model):
+     RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    Review = models.IntegerField(null=False)
-    Date = models.DateTimeField(null=False)
+    Review = models.IntegerField(choices=RATING_CHOICES)
+    #Review = models.IntegerField(null=False)
+    Date = models.DateTimeField(null=False, default=timezone.now)
+
     class Meta:
         unique_together = ['user', 'product']
+
 
 
 class Comments(models.Model):
