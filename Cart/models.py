07b4e1f865ea87b3d66fcc8ed3d45486dcecb541
HeBa <heba.Ibrahem.x@gmail.com>
2020-03-17 16:25:26 -0400

    Add files via upload
    Cart and Favorite pages with Delete and Move buttons
    -Issue in Favorite(move to cart ) button

--- Cart/models.py
+++ Cart/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 from products.models import Product
 from user.models import User
+from django.urls import reverse
 
 class Cart(models.Model):
     cartUser= models.ForeignKey(User,on_delete=models.CASCADE)
     cartProduct= models.ForeignKey(Product,on_delete=models.CASCADE)
     addingDate =models.DateField(auto_now= True)
+  
 
-    pass
+    
+    def productIdURLdel(self):
+
+        return reverse("cart:deleteCartItem",kwargs={'id': self.cartProduct.id })
+
+    def productIdURLmov(self):
+
+        return reverse("cart:moveToFav",kwargs={'id': self.cartProduct.id })
+
+
+class Favorite(models.Model):
+    userF= models.ForeignKey(User,on_delete=models.CASCADE)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    addDate =models.DateField(auto_now= True)
+
+    def pIdURLdel(self):
+
+        return reverse("cart:deleteFavItem",kwargs={'pid': self.product.id })
+
+    def ptIdURLmov(self):
+
+        return reverse("cart:moveToCart",kwargs={'pid': self.product.id })
+    
