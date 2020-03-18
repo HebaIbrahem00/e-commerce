06823df79e8a1e2f7c0d869c6e7c6217ec28ec42
BasantMahrous <35635869+BasantMahrous@users.noreply.github.com>
2020-03-19 01:18:17 +0200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
    
-    path('profile/<slug:slug>', views.profile, name='profile'),
+   # path('profile/<slug:slug>', views.profile, name='profile'),
     path('view_profile/', views.edit_profile, name='view_profile'),
     path('view_profile/edit_profile/', views.edit_profile, name='edit_profile')
 
