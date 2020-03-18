5c3e0cde651f058dbdb2a5f76fe7d57a262f5f08
BasantMahrous <35635869+BasantMahrous@users.noreply.github.com>
2020-03-19 01:28:53 +0200

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import ModelForm
+from accounts.models import Profile
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, help_text='Last Name')
@@ -11,3 +14,19 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+ class EditProfileForm(ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name'
+        )
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        # Note that we didn't mention user field here.
+        fields = ('first_name', 'last_name',
+                  'phone', 'address1', 'address2', 'image', 'creditNumber')
