00f0062ca32e0e29eb67adcc787e54606a01dd42
BasantMahrous <35635869+BasantMahrous@users.noreply.github.com>
2020-03-19 02:53:49 +0200

    add deactivate url and delete user url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
    
    # path('profile/<slug:slug>', views.profile, name='profile'),
     path('view_profile/', views.edit_profile, name='view_profile'),
-    path('view_profile/edit_profile/', views.edit_profile, name='edit_profile')
-
-
+    path('view_profile/edit_profile/', views.edit_profile, name='edit_profile'),
+    path('delete_profile/', views.delete_profile, name='delete_profile'),
+    path('delete_user/', views.delete_user, name='delete_user'),
 ]
