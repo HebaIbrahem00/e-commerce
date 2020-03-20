109dc0865220449c4efaa95a5f118f6abd45893b
BasantMahrous <35635869+BasantMahrous@users.noreply.github.com>
2020-03-20 22:33:34 +0200

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'crispy_forms',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
